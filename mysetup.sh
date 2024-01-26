c2cbcfebeadce014702c75c2a8f59abdfa783a05
Lykrin <piotr@egrodzki.com>
2024-01-26 14:15:26 +0100

    Initial commit

---
+++ mysetup.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+#   Check for Base Devel
+if pacman -Q | grep -q base-devel; then
+    echo -e "$COK - Base Devel was located, moving on."
+else
+    echo -e "$CER - Base Devel is required for this script, now exiting"
+    exit
+fi
+
+#   Check for yay
+ISYAY=/sbin/yay
+if [ -f "$ISYAY" ]; then 
+    echo -e "$COK - yay was located, moving on."
+    yay -Suy
+else 
+    echo -e "$CWR - Yay was NOT located"
+    read -n1 -rep $'[\e[1;33mACTION\e[0m] - Would you like to install yay (y,n) ' INSTYAY
+    if [[ $INSTYAY == "Y" || $INSTYAY == "y" ]]; then
+        git clone https://aur.archlinux.org/yay.git &>> $INSTLOG
+        cd yay
+        makepkg -si --noconfirm &>> ../$INSTLOG
+        cd ..
+        
+    else
+        echo -e "$CER - Yay is required for this script, now exiting"
+        exit
+    fi
+fi
+
+
+
+#   Install all needed packages
+read -n1 -rep 'Would you like to install the packages? (y,n)' INST
+if [[ $INST == "Y" || $INST == "y" ]]; then
+    yay -R --noconfirm swaylock waybar
+    yay -R --noconfirm swaylock waybar
+    yay -S hyprland-git polkit-gnome ffmpeg fastfetch neovim viewnior \
+    rofi-lbonn-wayland pavucontrol thunar starship cliphist wl-clipboard \
+    wf-recorder swww waypaper grimblast-git ffmpegthumbnailer tumbler gvfs \
+    playerctl noise-suppression-for-voice file-roller thunar-archive-plugin \
+    thunar-media-tags-plugin kitty thunar-volman gvfs-mtp \
+    waybar-git wlogout swaylock-effects pamixer papirus-icon-theme \
+    nwg-look-bin dunst ttf-firacode-nerd noto-fonts qt5-wayland qt6-wayland\
+    noto-fonts-emoji ttf-nerd-fonts-symbols-common otf-firamono-nerd \
+    brightnessctl hyprpicker-git pipewire lib32-pipewire wireplumber \
+    pipewire-audio pipewire-pulse pipewire-alsa pipewire-jack \
+    lib32-pipewire-jack xdg-user-dirs xdg-desktop-portal-hyprland catppuccin-gtk-theme-mocha --needed
+fi
+#   Making directory
+
+#xdg-user-dirs-update
+mkdir -p ~/Pictures/Screenshots/
+
+#   Copy Config Files
+read -n1 -rep 'Would you like to copy config files? (y,n)' CFG
+if [[ $CFG == "Y" || $CFG == "y" ]]; then
+    echo -e "Copying config files...\n"
+    cp -R config/dunst ~/.config/
+    cp -R config/hypr ~/.config/
+    cp -R config/kitty ~/.config/
+    #cp -R config/pipewire ~/.config/
+    cp -R config/rofi ~/.config/
+    cp -R config/swaylock ~/.config/
+    cp -R config/waybar ~/.config/
+    cp -R config/wlogout ~/.config/
+    cp -R config/xfce4 ~/.config/
+
+    # mkdir -p ~/Pictures/wallpaper
+    cp -R ./wallpapers ~/Pictures/
+    
+    # Set some files as exacutable 
+    chmod +x ~/.config/hypr/xdg-portal-hyprland
+    chmod +x ~/.config/waybar/scripts/*
+fi
+
+#   Install NVChad
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+
+
+#   Script is done
+echo -e "Script had completed.\n"
+echo -e "You can start Hyprland by typing Hyprland (note the capital H).\n"
+read -n1 -rep 'Would you like to start Hyprland now? (y,n)' HYP
+if [[ $HYP == "Y" || $HYP == "y" ]]; then
+    exec Hyprland
+else
+    exit
+fi
+
