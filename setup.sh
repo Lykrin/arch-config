8673c13f7477dcdce8cc7e8c73219a11b23d8642
Lykrin <piotr@egrodzki.com>
2024-07-08 11:25:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,90 +1,96 @@
 #!/bin/bash
+
 # Define ANSI color codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-#   Check for Base Devel
+INSTLOG="install.log"
+
+# Function to print colored messages
+print_message() {
+    local color=$1
+    local message=$2
+    echo -e "${color}${message}${NC}"
+}
+
+# Function to prompt user
+prompt_user() {
+    local prompt=$1
+    read -n1 -rep $"${YELLOW}${prompt}${NC} (Y/n) " response
+    [[ $response != "N" && $response != "n" ]]
+}
+
+# Check for Base Devel
 if pacman -Q | grep -q base-devel; then
-    echo -e "${GREEN}Base Devel was located, moving on.${NC}"
+    print_message "$GREEN" "Base Devel was located, moving on."
 else
-    echo -e "${RED}Base Devel is required for this script, now exiting${NC}"
-    exit
+    print_message "$RED" "Base Devel is required for this script, now exiting"
+    exit 1
 fi
 
 # Check for yay
-ISYAY=/sbin/yay
-INSTLOG="install.log"
-if [ -f "$ISYAY" ]; then 
-    echo -e "${GREEN}Yay was located, moving on.${NC}"
+if [ -f "/sbin/yay" ]; then 
+    print_message "$GREEN" "Yay was located, moving on."
     yay -Syu
 else 
-    echo -e "${RED}Yay was NOT located${NC}"
-    read -n1 -rep $'[\e[1;33mACTION\e[0m] - Would you like to install yay (Y/n) ' INSTYAY
-    if [[ $INSTYAY != "N" && $INSTYAY != "n" ]]; then
+    print_message "$RED" "Yay was NOT located"
+    if prompt_user "Would you like to install yay?"; then
         git clone https://aur.archlinux.org/yay.git &>> $INSTLOG
-        cd yay
-        makepkg -si --noconfirm &>> ../$INSTLOG
-        cd ..
+        (cd yay && makepkg -si --noconfirm) &>> $INSTLOG
         rm -rf yay
         yay -Syu --noconfirm
     else
-        echo -e "${RED}Yay is required for this script, now exiting${NC}"
-        exit
+        print_message "$RED" "Yay is required for this script, now exiting"
+        exit 1
     fi
 fi
 
-#   Install all needed packages
-read -n1 -rep $'Would you like to install the packages? (Y/n) ' INST
-if [[ $INST != "N" && $INST != "n" ]]; then
-    yay -S --noconfirm zsh hyprland-git polkit-gnome ffmpeg fastfetch neovim viewnior \
-    fuzzel pavucontrol zsh-antidote cliphist wl-clipboard nomacs clapper \
-    wf-recorder hyprpaper grimblast-git ffmpegthumbnailer btop gvfs \
-    foot foot-terminfo nemo nemo-fileroller gvfs-mtp brave-bin vesktop \
-    waybar-git hypridle starship hyprlock pamixer sweet-folders-icons-git LADSPA \
-    nwg-look-bin dunst ttf-firacode-nerd noto-fonts qt5-wayland qt6-wayland\
-    noto-fonts-emoji ttf-nerd-fonts-symbols-common otf-firamono-nerd \
-    brightnessctl pipewire noise-suppression-for-voice lib32-pipewire wireplumber \
-    pipewire-audio pipewire-pulse pipewire-alsa pipewire-jack ladspa motivewave \
-    lib32-pipewire-jack xdg-user-dirs xdg-desktop-portal-hyprland sweet-gtk-theme --needed
+# Install needed packages
+if prompt_user "Would you like to install the packages?"; then
+    yay -S --noconfirm --needed \
+        zsh hyprland-git polkit-gnome ffmpeg fastfetch neovim viewnior \
+        fuzzel pavucontrol zsh-antidote cliphist wl-clipboard nomacs clapper \
+        wf-recorder hyprpaper grimblast-git ffmpegthumbnailer btop gvfs \
+        foot foot-terminfo nemo nemo-fileroller gvfs-mtp brave-bin vesktop \
+        waybar-git hypridle starship hyprlock pamixer sweet-folders-icons-git LADSPA \
+        nwg-look-bin dunst ttf-firacode-nerd noto-fonts qt5-wayland qt6-wayland \
+        noto-fonts-emoji ttf-nerd-fonts-symbols-common otf-firamono-nerd \
+        brightnessctl pipewire noise-suppression-for-voice lib32-pipewire wireplumber \
+        pipewire-audio pipewire-pulse pipewire-alsa pipewire-jack ladspa motivewave \
+        lib32-pipewire-jack xdg-user-dirs xdg-desktop-portal-hyprland sweet-gtk-theme
 fi
 
-#  Copy .config files
-read -n1 -rep $'Would you like to copy .config files? (Y/n) ' CFG
-if [[ $CFG != "N" && $CFG != "n" ]]; then
-    echo -e "Copying .config files...\n"
+# Copy .config files
+if prompt_user "Would you like to copy .config files?"; then
+    print_message "$GREEN" "Copying .config files..."
     cp -R .config/* ~/.config/
-    # set as executable
-    chmod +x ~/.config/waybar/scripts/*
-    chmod +x ~/.config/hypr/xdg-portal-hyprland
+    chmod +x ~/.config/waybar/scripts/* ~/.config/hypr/xdg-portal-hyprland
 fi
 
-#   Install NVChad
-read -n1 -rep $'Would you like to install NVChad? (Y/n) ' NVCHAD
-if [[ $NVCHAD != "N" && $NVCHAD != "n" ]]; then
-    echo -e "Installing NVChad...\n"
+# Install NVChad
+if prompt_user "Would you like to install NVChad?"; then
+    print_message "$GREEN" "Installing NVChad..."
     git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 fi
 
-# set zsh as default bash
+# Set zsh as default shell
 chsh -s /bin/zsh
 
-#   Copy .zshrc and .zsh_plugins.txt
-read -n1 -rep $'Would you like to copy .zshrc and .zsh_plugins.txt? (Y/n) ' ZSH
-if [[ $ZSH != "N" && $ZSH != "n" ]]; then
-    echo -e "Copying .zshrc and .zsh_plugins.txt...\n"
+# Copy .zshrc and .zsh_plugins.txt
+if prompt_user "Would you like to copy .zshrc and .zsh_plugins.txt?"; then
+    print_message "$GREEN" "Copying .zshrc and .zsh_plugins.txt..."
     cp .zshrc ~/.zshrc
     cp .zsh_plugins.txt ~/.zsh_plugins.txt
-    # Ensure the files are readable
     chmod 644 ~/.zshrc ~/.zsh_plugins.txt
 fi
 
-#   Script is done
-echo -e "Script had completed. Remember to set wifi service up. https://wiki.archlinux.org/title/Lenovo_ThinkPad_T14s_(AMD)_Gen_3 \n"
-echo -e "You can start Hyprland by typing Hyprland (note the capital H).\n"
-read -n1 -rep $'Would you like to reboot? (Y/n) ' HYP
-if [[ $HYP != "N" && $HYP != "n" ]]; then
+# Script completion
+print_message "$GREEN" "Script has completed. Remember to set up wifi service: https://wiki.archlinux.org/title/Lenovo_ThinkPad_T14s_(AMD)_Gen_3"
+print_message "$GREEN" "You can start Hyprland by typing Hyprland (note the capital H)."
+
+# Prompt for reboot
+if prompt_user "Would you like to reboot?"; then
     reboot
-else
-    exit
 fi
