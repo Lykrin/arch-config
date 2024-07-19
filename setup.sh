fd44a188fe2de271b8db33707c2959083ed70035
Lykrin <piotr@egrodzki.com>
2024-07-19 08:50:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,15 +54,10 @@ if prompt_user "Would you like to install the packages?"; then
     {
         echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting package installation"
         yay -S --noconfirm --needed \
-            zsh hyprland polkit-gnome ffmpeg fastfetch neovim viewnior bolt-launcher \
-            fuzzel pavucontrol zsh-antidote cliphist wl-clipboard clapper wttrbar \
-            wf-recorder hyprpaper grimblast-git ffmpegthumbnailer btop gvfs vesktop \
-            foot foot-terminfo nemo nemo-fileroller gvfs-mtp brave-bin mkinitcpio-firmware \
-            waybar hypridle hyprlock pamixer sweet-folders-icons-git fprintd LADSPA \
-            nwg-look-bin dunst ttf-firacode-nerd noto-fonts qt5-wayland qt6-wayland \
-            noto-fonts-emoji ttf-nerd-fonts-symbols-common otf-firamono-nerd networkmanager-dmenu-git \
+           pipewire-jack zsh hyprland polkit-gnome ffmpeg fastfetch neovim viewnior bolt-launcher \
+ fuzzel pavucontrol zsh-antidote cliphist wl-clipboard clapper wttrbar \ wf-recorder hyprpaper grimblast-git ffmpegthumbnailer btop gvfs vesktop \ foot foot-terminfo nemo nemo-fileroller gvfs-mtp brave-bin mkinitcpio-firmware \ waybar hypridle hyprlock pamixer sweet-folders-icons-git fprintd LADSPA / nwg-look-bin dunst ttf-firacode-nerd noto-fonts qt5-wayland qt6-wayland \ noto-fonts-emoji ttf-nerd-fonts-symbols-common otf-firamono-nerd networkmanager-dmenu-git \
             brightnessctl pipewire noise-suppression-for-voice lib32-pipewire wireplumber \
-            pipewire-audio pipewire-pulse pipewire-alsa pipewire-jack ladspa motivewave \
+            pipewire-audio pipewire-pulse pipewire-alsa ladspa motivewave \
             lib32-pipewire-jack xdg-user-dirs xdg-desktop-portal-hyprland sweet-gtk-theme \
             network-manager-applet cava
         echo "[$(date '+%Y-%m-%d %H:%M:%S')] Package installation completed"
