a1edaa6c301073c2f15c6a900b4f1416d732a0c1
Lykrin <piotr@egrodzki.com>
2025-10-10 22:32:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -83,23 +83,23 @@ if prompt_user "Would you like to install the packages?"; then
     base_deps=(cmake ninja meson wayland-protocols libxcb xcb-proto xcb-util xcb-util-keysyms
                libxfixes libx11 libxcomposite xorg-xinput libxrender pixman libdrm libxkbcommon
                xcb-util-wm xorg-xwayland glslang qt6-wayland pugixml)
-    install_packages "" "${base_deps[@]}"
+    install_packages "--sudoloop --removemake --cleanafter" "${base_deps[@]}"
     
     # Audio stack
     audio_deps=(pipewire pipewire-pulse pipewire-alsa pipewire-jack pipewire-audio
                 lib32-pipewire lib32-pipewire-jack wireplumber noise-suppression-for-voice)
-    install_packages "" "${audio_deps[@]}"
+    install_packages "--sudoloop --removemake --cleanafter" "${audio_deps[@]}"
     
     # Git packages with overwrite option
-    git_packages=(hyprutils-git hyprlang-git hyprwayland-scanner-git hyprland-protocols-git
-                  hyprgraphics-git hyprland-qt-support-git hyprland-qtutils-git hyprcursor-git
-                  aquamarine-git hyprland-git)
-    install_packages "--overwrite='*'" "${git_packages[@]}"
+    hypr_packages=(hyprutils hyprlang hyprwayland-scanner hyprland-protocols
+                  hyprgraphics hyprland-qt-support hyprland-qtutils hyprcursor
+                  aquamarine hyprland)
+    install_packages "--sudoloop --removemake --cleanafter" "${hypr_packages[@]}"
     
     # Additional utilities with overwrite option
-    additional_utils=(hypridle-git hyprlock-git hyprpaper-git hyprpolkitagent-git
-                      xdg-desktop-portal-hyprland-git)
-    install_packages "--overwrite='*'" "${additional_utils[@]}"
+    hypr_utils=(hypridle hyprlock hyprpaper hyprpolkitagent
+                      xdg-desktop-portal-hyprland)
+    install_packages "--sudoloop --removemake --cleanafter" "${hypr_utils[@]}"
     
     # Rest of the packages - removed NetworkManager and added iwd for iwctl
     other_packages=(fish waybar iwd ffmpeg ffmpegthumbnailer
@@ -109,9 +109,9 @@ if prompt_user "Would you like to install the packages?"; then
                     sweet-folders-icons-git xdg-user-dirs fastfetch ladspa noto-fonts-cjk ttf-firacode-nerd noto-fonts
                     noto-fonts-emoji steam ttf-nerd-fonts-symbols-common otf-firamono-nerd qt5-wayland
                     qt6-wayland mkinitcpio-firmware ib-tws nwg-look bolt-launcher bibata-cursor-theme-bin
-                    gnome-themes-extra qt5ct qt6ct ripgrep xsettingsd xdg-desktop-portal xdg-desktop-portal-gtk
+                    gnome-themes-extra qt5ct qt6ct ripgrep xdg-desktop-portal xdg-desktop-portal-gtk
                     qbittorrent-enhanced xsettingsd breeze-icons jre11-openjdk libva lib32-vulkan-mesa-layers libvdpau-va-gl)
-    install_packages "" "${other_packages[@]}"
+    install_packages "--sudoloop --removemake --cleanafter" "${other_packages[@]}"
     
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] Package installation completed" | tee -a "$INSTLOG"
 fi
